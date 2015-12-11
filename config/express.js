572e05725ee1f0b46ef52b254080a1b46e367bff
thisdotrob <thisdotrob@gmail.com>
2015-12-11 17:44:09 +0000

    Add EJS & express dependencies, update node config

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 module.exports = function() {
   var app = express();
+  app.use(express.static('./public'));
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
   require('../app/routes/index.server.routes.js')(app);
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var config = require('./config/config');
 var express = require('./config/express');
 var app = express();
-app.listen(config.port);
+var port = config.port;
+var nodeEnv = process.env.NODE_ENV;
+app.listen(port);
+console.log(nodeEnv + ' server running at http://localhost:' + port);
