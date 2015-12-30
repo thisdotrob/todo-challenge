b247ef9f472bd91e7a487081b831e5ab2b2c6306
thisdotrob <thisdotrob@gmail.com>
2015-12-30 23:49:21 +0000

    Update readme, gulp testing tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,43 @@ var server = require('gulp-develop-server');
 var karmaServer = require('karma').Server;
 var mocha = require('gulp-mocha');
 
-gulp.task('e2e', function() {
-  server.listen( { path: './server.js' } );
+gulp.task('listen', ['set-env'], function(done) {
+  var opts = {
+    path: './server.js',
+    env: {NODE_ENV: 'test'}
+  };
+  server.listen(opts , function(err) {
+    done(err);
+  });
+})
+
+gulp.task('feature-test', ['listen'], function() {
   gulp.src(["./test/e2e/feature.js"])
       .pipe(protractor({
         configFile: "test/e2e/conf.js",
         args: ['--baseUrl', 'http://127.0.0.1:8080']
       }))
       .on('error', function(e) { throw e })
-})
+      .once('end', function() {
+        server.kill();
+      })
+});
 
-gulp.task('karma', function() {
+gulp.task('node-test', ['listen'], function() {
+  gulp.src(['./test/unit/node/spec.*.js'], {read: false})
+    .pipe(mocha())
+    .once('end', function() {
+      server.kill();
+    });
+});
+
+gulp.task('angular-test', function() {
   new karmaServer({
     configFile: __dirname + '/test/unit/angular/karma-conf.js',
     singleRun: true
   }).start();
 });
 
-gulp.task('mocha', function() {
-  return gulp.src('./test/unit/node/spec.*.js', {read: false})
-    .pipe(mocha());
-})
-
-gulp.task('unit', ['karma', 'mocha']);
+gulp.task('set-env', function() {
+  process.env.NODE_ENV = 'test';
+});
